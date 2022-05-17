89992864a06c0c9765a50abc2150204f1a0c9c46
Acadia Huber <horsinaround235@gmail.com>
2022-05-17 08:49:29 -0400

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?")
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 console.log(playerName, playerAttack, playerHealth);
 
@@ -12,8 +13,11 @@ var fight = function() {
     window.alert("Welcome to Robot Gladiators!");
 };
 
+var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
 fight();
 
+if (promptFight === "fight" || promptFight === "FIGHT") {
 enemyHealth = enemyHealth - playerAttack;
 
 console.log(
@@ -39,4 +43,19 @@ if (playerHealth <= 0) {
 }
 else {
     window.alert(playerName + " still has " + playerHealth + " health left.");
+}
+
+} else if (promptFight === "skip" || promptFight === "SKIP") {
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    if (confirmSkip) {
+        window.alert(playerName + " has decided to skip this fight. Goodbye!");
+        playerMoney = playerMoney - 2;
+        console.log
+    }
+    else{
+        fight();
+    }
+} else {
+    window.alert("You need to choose a valid option. Try again!");
 }
